6a9cf52cffc1390702cb43afee3dd790ebb6954a
CasperNEW <CasperNEW@inbox.ru>
2021-08-26 19:06:57 +0300

    Initial commit

---
+++ RSSchool_T10/RSSchool_T10/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RSSchool_T10
+//
+//  Created by Дмитрий Константинов on 26.08.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = ViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
+
